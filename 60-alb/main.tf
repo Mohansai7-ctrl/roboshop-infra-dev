2c870161e160dce970db3e13f59e5a3c75919811
Mohansai <bandarumohansai@gmail.com>
2024-12-08 14:42:59 +0530

    updated public key in eks

--- 60-alb/main.tf
+++ 60-alb/main.tf
@@ -52,10 +52,10 @@ resource "aws_lb_listener" "https" {
 module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
 
-  zone_name = var.zone_name #daws81s.online
+  zone_name = var.zone_name #mohansai.online
   records = [
     {
-      name    = "roboshop-${var.environment}" # roboshop-dev.daws81s.online
+      name    = "roboshop-${var.environment}" # roboshop-dev.mohansai.online
       type    = "A"
       alias   = {
         name    = module.ingress_alb.dns_name
@@ -96,7 +96,7 @@ resource "aws_lb_listener_rule" "frontend" {
 
   condition {
     host_header {
-      values = ["roboshop-${var.environment}.${var.zone_name}"] #roboshop-dev.daws81s.online
+      values = ["roboshop-${var.environment}.${var.zone_name}"] #roboshop-dev.mohansai.online
     }
   }
 }
